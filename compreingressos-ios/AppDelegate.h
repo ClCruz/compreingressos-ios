630e97409bf2f19dc38d3df30a13049b91b76e16
Robinson Nakamura <robmnk@gmail.com>
2015-03-16 13:31:14 -0300

    First commit

---
+++ compreingressos-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  compreingressos-ios
+//
+//  Created by Robinson Nakamura on 3/16/15.
+//  Copyright (c) 2015 QPRO Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ compreingressos-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  compreingressos-ios
+//
+//  Created by Robinson Nakamura on 3/16/15.
+//  Copyright (c) 2015 QPRO Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
